21868f2ecdfebb372c0de6cf68c184a4a9268ea6
Gabriel Caiana Guedes <gabrielcaianaguedes@gmail.com>
2019-01-28 14:06:42 -0200

    criando array para armazenar dias/Meses

--- js/app.js
+++ js/app.js
@@ -29,7 +29,14 @@
 
   let expirarMensagem; // Mensagem exibida  ao usuário
   let hoje; // Data de hoje
-  let elTermino // O elemento que exibe a mensagem sobre o fim da oferta 
+  let elTermino // O elemento que exibe a mensagem sobre o fim da oferta
 
-
+  function ofertaExpira(hoje) {
+      //Declara as variaveis dentro da função, para escopo local
+      weekFromToday = dia, data, mes, ano, diasNome, mesNomes;
+      //Adiciona 7 dias a data/hora atual (adicionado em millisegundos)
+      weekFromToday = new Date(hoje.getTime() + 7 * 24 * 60 * 60 * 1000);
+      //Cria um array para armazenar os nomes dos dias/mesNomes
+      diasNome = ['Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sabado', 'Domingo']; 
+  }
   }())
