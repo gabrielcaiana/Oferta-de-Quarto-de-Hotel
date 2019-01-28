9c2546e4203adbe6665bdd22587bd79733ff1ff2
Gabriel Caiana Guedes <gabrielcaianaguedes@gmail.com>
2019-01-28 13:06:47 -0200

    criando objeto e detalhes da oferta

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 /* O Script é colocado dentro de uma expressão de função imediatamente invocada que ajuda a proteger o escopo de variaveis */
+(function(){
+  //PARTE 1: CRIA O OBJETO HOTEL E EXIBE OS DETALHES DA OFERTA
+
+  //Cria o objeto hotel usando a sintaxe literal
+  let hotel =  {
+    nome: 'CaianaHotel',
+    tarifaQuarto: 100, // Quantia em real
+    desconto: 15, // Porcentagem de desconto
+    precoOferta: function () {
+      let taxaOferta = this.tarifaQuarto * ((100 - this.desconto) / 100);
+      return precoOferta;
+    }
+  }
+})
